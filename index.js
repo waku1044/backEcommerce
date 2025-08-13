ae1e5810220b6b3a39e30a586a75851ec42b5104
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2025-08-13 03:31:19 -0300

    se modifivo vercel

--- index.js
+++ index.js
@@ -11,15 +11,29 @@ import rutasClients from './rutas/clients.routes.js';
 // const archivoDB = require("./conexion");
 import conectarDB from './db/conexion.js';
 await conectarDB();
-
 const app = express();
+
+const listaOrigin = ['http://localhost:5173','https://back-ecommerce-olive.vercel.app/']// o una lista de dominios permitidos
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Permite solicitudes sin origin (por ejemplo, desde herramientas como Postman)
+    if (!origin || listaOrigin.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origen no permitido por CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
 const port = process.env.PORT || 3000 ;
 // Habilitar CORS para todas las rutas
 
 
 
 app.use(express.json());
-app.use(cors());
 
 
 
